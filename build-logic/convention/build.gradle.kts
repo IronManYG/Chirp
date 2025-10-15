91ad8070fcceb01810b4705b0aa364b1ee1239a2
Hussain Gaddal <hussain.yg2010@gmail.com>
2025-10-16 01:53:48 +0300

    feat: Add Room convention plugin
    - Introduce `RoomConventionPlugin` for configuring Room and KSP in Kotlin Multiplatform projects.
    - Register plugin `dev.gaddal.convention.room` in `build-logic/convention`.
    - Update `libs.versions.toml` with Room and KSP plugin aliases.
    - Apply `RoomConventionPlugin` to `feature/chat/database` module.

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     compileOnly(libs.kotlin.gradlePlugin)
     compileOnly(libs.compose.gradlePlugin)
     compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.androidx.room.gradle.plugin)
     implementation(libs.buildkonfig.gradlePlugin)
     implementation(libs.buildkonfig.compiler)
 }
@@ -69,6 +70,11 @@ gradlePlugin {
         register("buildKonfig") {
             id = "dev.gaddal.convention.buildkonfig"
             implementationClass = "BuildKonfigConventionPlugin"
+        }
+
+        register("room") {
+            id = "dev.gaddal.convention.room"
+            implementationClass = "RoomConventionPlugin"
         }
     }
 }
