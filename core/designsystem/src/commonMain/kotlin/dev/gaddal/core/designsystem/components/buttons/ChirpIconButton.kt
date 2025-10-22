e5c5e88c020c79772ddc65ce17b87ad29a6f4efb
Hussain Gaddal <hussain.yg2010@gmail.com>
2025-10-22 16:33:51 +0300

    feat(designsystem): Add `ChirpIconButton` composable
    Introduces a new `ChirpIconButton`, a custom-styled `OutlinedIconButton`.
    
    - The button has a fixed size of `45.dp`, rounded corners, and a 1dp border.
    - It uses `MaterialTheme.colorScheme.surface` for the container and a custom `textSecondary` color for the content.
    - Includes `@Preview` composables for both light and dark themes.

---
+++ core/designsystem/src/commonMain/kotlin/dev/gaddal/core/designsystem/components/buttons/ChirpIconButton.kt
@@ -0,0 +1,78 @@
+package dev.gaddal.core.designsystem.components.buttons
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedIconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.gaddal.core.designsystem.theme.ChirpTheme
+import dev.gaddal.core.designsystem.theme.extended
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+/**
+ * A composable function that renders a custom-styled outlined icon button.
+ *
+ * @param onClick The callback to be invoked when the button is clicked.
+ * @param modifier The modifier to be applied to the button. Defaults to [Modifier].
+ * @param content The composable content to be displayed inside the button.
+ */
+@Composable
+fun ChirpIconButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    OutlinedIconButton(
+        onClick = onClick,
+        modifier = modifier
+            .size(45.dp),
+        shape = RoundedCornerShape(8.dp),
+        border = BorderStroke(
+            width = 1.dp,
+            color = MaterialTheme.colorScheme.outline
+        ),
+        colors = IconButtonDefaults.outlinedIconButtonColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.extended.textSecondary
+        )
+    ) {
+        content()
+    }
+}
+
+@Composable
+@Preview
+fun ChirpButtonPreview() {
+    ChirpTheme {
+        ChirpIconButton(
+            onClick = {}
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun ChirpButtonDarkThemePreview() {
+    ChirpTheme(darkTheme = true) {
+        ChirpIconButton(
+            onClick = {}
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = null
+            )
+        }
+    }
+}
